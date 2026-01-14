25f2795bbd222a438d56a9a5d7144e24726c6fab
Dr Xenon <drxenon487@gmail.com>
2026-01-14 09:07:18 +0000

    Update xenon.js

--- scripts/data/xenon.js
+++ scripts/data/xenon.js
@@ -1,1 +1,11 @@
 // xenon.js - Temporary file for data\n// Created: Fri Jan  2 05:30:41     2026\nconsole.log('xenon.js loaded for data');
+# Clés API (dans .env.local)
+STATSBOMB_API_KEY="your_key"
+OPTA_API_KEY="your_key"
+TWITTER_API_KEY="your_key"
+NEWSAPI_API_KEY="your_key"
+
+# Configuration
+NOTIFICATION_WEBHOOK="https://hooks.slack.com/..."
+DATA_RETENTION_DAYS=90
+MAX_PARALLEL_JOBS=5
