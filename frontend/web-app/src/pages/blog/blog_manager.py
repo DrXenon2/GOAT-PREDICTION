bb67a35b1aae9d740a4c277b2184faf4871562e8
Dr Xenon <drxenon487@gmail.com>
2026-01-31 16:21:33 +0000

    Add files via upload

---
+++ frontend/web-app/src/pages/blog/blog_manager.py
@@ -0,0 +1,12 @@
+"""
+blog_manager
+Blog manager
+"""
+
+def main():
+    """Main function"""
+    print("blog_manager loaded")
+
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/web-app/src/pages/page_manager.py
@@ -0,0 +1,12 @@
+"""
+page_manager
+Page manager
+"""
+
+def main():
+    """Main function"""
+    print("page_manager loaded")
+
+
+if __name__ == "__main__":
+    main()
